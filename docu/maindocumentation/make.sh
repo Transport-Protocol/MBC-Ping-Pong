fe571e11ce42ff5907781c5725641bf4ff87d1e9
TorbenHaug <claustorbenhaug@hotmail.de>
2016-12-07 18:22:17 +0100

    fix make.sh

--- docu/maindocumentation/make.sh
+++ docu/maindocumentation/make.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-pdflatex MBC-Ping-Pong
+pdflatex -synctex=1 -interaction=nonstopmode "MBC-Ping-Pong".tex
 state=$?
 if [ ! $state = "0" ]; then
 	echo "Failed to compile latex!" 1>&2
 	exit $state
 fi
 
-
-bibtex MBC-Ping-Pong
-state=$?
-if [ ! $state = "0" ]; then
-	echo "Failed to compile bibtex!" 1>&2
-	exit $state
-fi
-
-pdflatex MBC-Ping-Pong
+pdflatex -synctex=1 -interaction=nonstopmode "MBC-Ping-Pong".tex
 state=$?
 if [ ! $state = "0" ]; then
 	echo "Failed to compile latex!" 1>&2
 	exit $state
 fi
-
