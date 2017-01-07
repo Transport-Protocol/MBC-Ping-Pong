9c863cc1f19381f97f5ae03f4c56dc46b068e89a
Shivon <marjan.bachtiari@haw-hamburg.de>
2017-01-07 04:30:02 +0100

    Make control relative

--- src/public/js/FingerPositionHandler.js
+++ src/public/js/FingerPositionHandler.js
@@ -7,23 +7,45 @@ module.exports = FingerPositionHandler;
   *
   * Angelehnt an: https://mobiforge.com/design-development/html5-mobile-web-touch-events
   */
+
 function FingerPositionHandler(canvas, interval, callbackFunctions){
   var _canvas = canvas;
   var _interval = interval;
   var _callbackFunctions = callbackFunctions;
   var _touchzone = document.getElementById(canvas);
-  var _x = 0;
-  var _y = 0;
+  var _paddle_x = 0;
+  var _paddle_y = 0;
+  var _player_start_x = 0;
+  var _player_start_y = 0;
+  var _change_x = 0;
+  var _change_y = 0;
   var _lastSendTime = undefined;
   var _lastSendDiff = 0;
   var _processMouseInformations = false;
 
   /**
-    * Wird aufgerufen, wenn die Mouse- oder Toucheingabe startet
+    * Differentiates between touch and mouse
+    * Returns coordinate for given axis
+    */
+  function axis(event, axis) {
+    if (event.touches) {
+      return event.touches[0][axis];
+    }
+    else {
+      return event[axis];
+    }
+  }
+
+  /**
+    * Gets called when input (mouse/ touch) starts
     */
   function processInputStart(event){
     _processMouseInformations = true;
-    // Prevent iPad from scrolling
+    _player_start_x = axis(event, 'pageX');
+    _player_start_y = axis(event, 'pageY');
+
+    console.log("relative touch start", _player_x, _player_y);
+    // TODO: Prevent iPad from scrolling, the one below doesnt really work
     event.preventDefault();
   }
 
@@ -34,28 +56,28 @@ function FingerPositionHandler(canvas, interval, callbackFunctions){
     if(_processMouseInformations){
       if(_touchzone.getContext){
         var ctx = _touchzone.getContext("2d");
-        if(event.touches){
-          _x = event.touches[0].pageX - _offset.left;
-          _y = event.touches[0].pageY - _offset.top;
-        }
-        else{
-          _x = event.pageX - _offset.left;
-          _y = event.pageY - _offset.top;
-        }
+
+        _change_x = axis(event, 'pageX') - _player_start_x;
+        _change_y = axis(event, 'pageY') - _player_start_y;
+
+        console.log("CHANGE", _change_x, _change_y);
+        var _x = _paddle_x + _change_x;
+        var _y = _paddle_y + _change_y;
+
         ctx.fillRect(_x, _y, 5, 5);
         var currentTime = new Date().getTime();
-        if((currentTime - _lastSendTime) >= (_interval - _lastSendDiff) || _lastSendTime === undefined){
+        // if((currentTime - _lastSendTime) >= (_interval - _lastSendDiff) || _lastSendTime === undefined){
           for (var i = 0, len = callbackFunctions.length; i < len; i++) {
             callbackFunctions[i](_x, _y);
           }
-          if(_lastSendTime){
-            _lastSendDiff = (currentTime - _lastSendTime) - _interval;
-            if(_lastSendDiff < 0){
-              _lastSendDiff = 0;
-            }
-          }
-          _lastSendTime = currentTime;
-        }
+        //   if(_lastSendTime){
+        //     _lastSendDiff = (currentTime - _lastSendTime) - _interval;
+        //     if(_lastSendDiff < 0){
+        //       _lastSendDiff = 0;
+        //     }
+        //   }
+        //   _lastSendTime = currentTime;
+        // }
       }
     }
     // Prevent iPad from scrolling
@@ -70,6 +92,8 @@ function FingerPositionHandler(canvas, interval, callbackFunctions){
     _lastSendTime = undefined;
     _lastSendDiff = 0;
     _processMouseInformations = false;
+    _paddle_x += _change_x;
+    _paddle_y += _change_y;
     // Prevent iPad from scrolling
     event.preventDefault();
   }
