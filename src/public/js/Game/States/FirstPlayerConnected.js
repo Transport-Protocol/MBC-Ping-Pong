8db5b6c5a0bbc029928211ebfff8dbb1884e49c4
TorbenHaug <claustorbenhaug@hotmail.de>
2017-01-08 16:50:52 +0100

    #27 add timer

--- src/public/js/Game/States/FirstPlayerConnected.js
+++ src/public/js/Game/States/FirstPlayerConnected.js
@@ -2,6 +2,7 @@ var abstractState = require('./abstractState.js');
 var Ball = require('./../Ball.js');
 var Player = require('./../Player.js');
 
+const TIME_TO_CONNECT_SECOND_PLAYER_IN_SECONDS = 30;
 var state = function(game){
   var self = this;
   self.game = game;
@@ -11,13 +12,31 @@ var state = function(game){
   	console.log("FirstPlayerConnected");
   };
   this.preload = function(){
+    self.circle = game.add.sprite(game.width/2 - 25,game.height/2 - 100,'timerBackground');
 
+    //create Score fpr Player 0
+    var timerTextStyle = { font: "bold 32px Arial", fill: "#FF0000", boundsAlignH: "center", boundsAlignV: "middle" };
+    self.timerText = game.add.text(0, 0, "" + TIME_TO_CONNECT_SECOND_PLAYER_IN_SECONDS, timerTextStyle);
+    self.timerText.setTextBounds(self.circle.x, self.circle.y, self.circle.width, self.circle.height);
+  };
+  this.create = function(){
+    self.timer = game.time.create();
+    self.timerEvent = self.timer.add(Phaser.Timer.SECOND * TIME_TO_CONNECT_SECOND_PLAYER_IN_SECONDS, function(){
+      this.game.state.states[this.game.state.current].timeout();
+    }, this);
+    self.timer.start();
   };
   this.addPlayer = function(){
     var player = new Player(self.game, self.wallBuilder.getPlayerInfoPack(1), 'paddle', 1);
     this.game.state.start("SecondPlayerConnected", false, false);
     return player;
-  }
+  };
+  //this.timeout = function(){
+    //this.game.state.start("InitializeNewGame", false, false);
+  //};
+  this.render = function(){
+    self.timerText.text = Math.round(self.timer.duration / 1000);
+  };
 };
 
 state.prototype = Object.create(abstractState.prototype);
