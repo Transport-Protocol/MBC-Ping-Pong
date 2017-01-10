b94a70c5e475b4d1a81f5602a22f212cbd140754
TorbenHaug <claustorbenhaug@hotmail.de>
2017-01-10 20:08:50 +0100

    #32 add start stop and reset function to ball

--- src/public/js/Game/Ball.js
+++ src/public/js/Game/Ball.js
@@ -1,12 +1,29 @@
 
 var Ball = function (game, x, y) {
+  var self = this;
   Phaser.Sprite.call(this, game, x, y, 'ball');
+  var initialX = x;
+  var initialY = y;
 
-
+  var generateRandomDirection = function(){
+    var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+    return plusOrMinus * ((Math.random() * (0.6 * Math.PI)) + (0.2 * Math.PI))
+  }
   game.physics.p2.enable(this, this.game.properties.debug);
-  this.body.setCircle(16);
-  this.body.angle = 80;
+  self.body.setCircle(16);
 
+  this.start = function(){
+    self.body.rotation = generateRandomDirection();
+    self.body.thrust(10000);
+  };
+  this.stop = function(){
+    self.body.thrust(0);
+  };
+  this.reset = function(){
+    self.body.x = initialX;
+    self.body.y = initialY;
+    this.start();
+  };
 
 }
 
